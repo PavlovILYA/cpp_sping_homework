eb1509883307559292e77ad2e3c65b16819c503b
Илюша <38328771+PavlovILYA@users.noreply.github.com>
2019-04-01 16:37:10 +0300

    beta3

--- Homework_03/BigInt.cpp
+++ Homework_03/BigInt.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string> 
+#include <cmath>                    // для функции ceil
 
 using namespace std;
 
@@ -30,17 +31,6 @@ private:
 	int* number;
 	bool sign;
 
-public:
-	BigInt()
-	{
-		number = new int[maxLen + 1];
-		for (int i = 1; i <= maxLen; i++)
-		{
-			number[i] = 0;
-		}
-		number[0] = 1;
-	}
-
 	void strToLong(string str)  // используется в конструкторе
 	{
 		if (str[0] != '-')
@@ -63,6 +53,93 @@ public:
 		}
 		cur = str.substr(0, str.size() - (i - 1)*baseLen);
 		number[number[0]] = atoi(cur.c_str());
+	}
+
+	BigInt longSum(const BigInt &B)  // используется в перегрузке операторов +=, +
+	{
+		BigInt C;
+		C.sign = B.sign;
+		int m = max(number[0], B.number[0]);
+		int buffer = 0;
+		for (int i = 1; i <= m; i++)
+		{
+			C.number[i] = number[i] + B.number[i] + buffer;
+			buffer = 0;
+			if (C.number[i] >= base)
+			{
+				C.number[i] -= base;
+				buffer = 1;
+			}
+			if (!C.number[m + 1])
+				C.number[0] = m;
+			else
+				C.number[0] = m + 1;
+		}
+		return C;
+	}
+
+	BigInt longDif(const BigInt &B)      // используется в перегрузке операторов -, +
+	{									// заведомо this больше B !!!  // &B or B ??
+		BigInt C;
+		C.sign = true;
+		int m = number[0];
+		int buffer = 0;
+		for (int i = 1; i <= m; i++)
+		{
+			if (number[i] < B.number[i])
+			{
+				number[i] += base;
+				buffer = 1;
+			}
+			C.number[i] = number[i] - B.number[i];
+			number[i + 1] -= buffer;
+			buffer = 0;
+		}
+		if (!C.number[m])
+			while (!C.number[m] && m > 1)
+				m--;
+		C.number[0] = m;
+		return C;
+	}
+
+	int longCmp(const BigInt &B) const   // используется в перегрузке операторов сравнения
+	{
+		if ((number[0] == 1) && (number[1] == 0) && (B.number[0] == 1) && (B.number[1] == 0)) return 0; // равенство положительного и отрицательного нуллей
+		if (sign && !B.sign) return 1;  //первое "+", второе "-"
+		if (!sign && B.sign) return -1; //первое "-", второе "+"
+		if (sign && B.sign)             //оба "+"
+		{
+			if (number[0] > B.number[0]) return 1;
+			if (number[0] < B.number[0]) return -1;
+			for (int i = number[0]; i >= 1; i--)
+			{
+				if (number[i] > B.number[i]) return 1;
+				if (number[i] < B.number[i]) return -1;
+			}
+			return 0;
+		}
+		if (!sign && !B.sign)           //оба "-"
+		{
+			if (number[0] > B.number[0]) return -1;
+			if (number[0] < B.number[0]) return 1;
+			for (int i = number[0]; i >= 1; i--)
+			{
+				if (number[i] > B.number[i]) return -1;
+				if (number[i] < B.number[i]) return 1;
+			}
+			return 0;
+		}
+	}
+
+public:
+	BigInt()
+	{
+		number = new int[maxLen + 1];
+		for (int i = 1; i <= maxLen; i++)
+		{
+			number[i] = 0;
+		}
+		number[0] = 1;
 	}
 
 	BigInt(int64_t num)
@@ -91,53 +168,6 @@ public:
 		return *this;
 	}
 
-	BigInt longDif(const BigInt B) // заведомо this больше B !!!  // &B or B ??
-	{
-		BigInt C;
-		C.sign = true;
-		int m = number[0];
-		int buffer = 0;
-		for (int i = 1; i <= m; i++)
-		{
-			if (number[i] < B.number[i])
-			{
-				number[i] += base;
-				buffer = 1;
-			}
-			C.number[i] = number[i] - B.number[i];
-			number[i + 1] -= buffer;
-			buffer = 0;
-		}
-		if (!C.number[m])
-			while (!C.number[m] && m > 1)
-				m--;
-		C.number[0] = m;
-		return C;
-	}
-
-	BigInt longSum(const BigInt B)
-	{
-		BigInt C;
-		C.sign = B.sign;
-		int m = max(number[0], B.number[0]);
-		int buffer = 0;
-		for (int i = 1; i <= m; i++)
-		{
-			C.number[i] = number[i] + B.number[i] + buffer;
-			buffer = 0;
-			if (C.number[i] >= base)
-			{
-				C.number[i] -= base;
-				buffer = 1;
-			}
-			if (!C.number[m + 1])
-				C.number[0] = m;
-			else
-				C.number[0] = m + 1;
-		}
-		return C;
-	}
-
 	BigInt &operator+=(const BigInt &B)
 	{
 		if (this->sign == B.sign)
@@ -146,7 +176,11 @@ public:
 			if ((this->sign == true) && (B.sign == false))
 				*this = this->longDif(B);
 			else
-				*this = B.longDif(*this);
+			{ 
+				BigInt C = *this; 
+				BigInt D = B;
+				*this = D.longDif(C);          // изначально было *this = B.longDif(*this), но ругалось
+			}
 		return *this;
 	}
 
@@ -155,35 +189,6 @@ public:
 		BigInt C = *this;
 		C.sign = !sign;
 		return C;
-	}
-
-	int longCmp(BigInt B)
-	{
-		if ((number[0] == 1) && (number[1] == 0) && (B.number[0] == 1) && (B.number[1] == 0)) return 0; // равенство положительного и отрицательного нуллей
-		if (sign && !B.sign) return 1;  //первое "+", второе "-"
-		if (!sign && B.sign) return -1; //первое "-", второе "+"
-		if (sign && B.sign)             //оба "+"
-		{
-			if (number[0] > B.number[0]) return 1;
-			if (number[0] < B.number[0]) return -1;
-			for (int i = number[0]; i >= 1; i--)
-			{
-				if (number[i] > B.number[i]) return 1;
-				if (number[i] < B.number[i]) return -1;
-			}
-			return 0;
-		}
-		if (!sign && !B.sign)           //оба "-"
-		{
-			if (number[0] > B.number[0]) return -1;
-			if (number[0] < B.number[0]) return 1;
-			for (int i = number[0]; i >= 1; i--)
-			{
-				if (number[i] > B.number[i]) return -1;
-				if (number[i] < B.number[i]) return 1;
-			}
-			return 0;
-		}
 	}
 
 	bool operator<(const BigInt &B) const    // (1)  BigInt
@@ -271,7 +276,8 @@ public:
 	}
 
 
-}
+};
+
 
 int main()
 {
